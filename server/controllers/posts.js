b2b674a58fc1f3217dfd42dd04da780f295d6261
iamDrewFreedman <exodus23_1@protonmail.com>
2022-09-21 16:13:01 -0400

    added material ui

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-export const getPosts = (req, res) => {
-    res.send('this works');
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
-}
 
+export const createPost = async (req, res) => {
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
 
 import PostMessage from '../models/postMessage.js';
 /*
