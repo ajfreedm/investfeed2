563fbec147f878d4583a821b6f728b4e54bcc34a
iamDrewFreedman <exodus23_1@protonmail.com>
2022-09-21 15:00:33 -0400

    added model

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import cors from 'cors';
 
 // import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
+import { getPosts, createPost } from '../controllers/posts.js';
+
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('hello world')
-  })
+router.get('/', getPosts);
+router.post('/', createPost);
+
+/*
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+*/
 
 export default router;
