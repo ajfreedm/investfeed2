6ad2182f989e8459079c444de961068c954cacc5
iamDrewFreedman <exodus23_1@protonmail.com>
2022-10-04 17:19:29 -0400

    more material fixing

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
-// import useStyles from './styles';
+import useStyles from './styles';
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
 
-    // const classes = useStyles();
+    const classes = useStyles();
 
     console.log(posts);
 
