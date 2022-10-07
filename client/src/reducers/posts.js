62c5cca696993b97042709ce5cd58ab93980c49e
iamDrewFreedman <exodus23_1@protonmail.com>
2022-10-07 14:49:08 -0400

    installed dotenv

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,7 @@
-/* import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+/*
+
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
 
 export default (posts = [], action) => {
   switch (action.type) {
@@ -22,15 +25,15 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionType
 
 export default (posts = [], action) => {
   switch (action.type) {
-      case 'UPDATE':
+      case UPDATE:
           return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-      case 'FETCH_ALL':
+      case FETCH_ALL:
           return action.payload;
-      case 'CREATE':
+      case CREATE:
           return [...posts, action.payload];
-      case 'DELETE':
+      case DELETE:
           return posts.filter((post) => post._id !== action.payload);
-      case 'LIKE':
+      case LIKE:
           return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
       default:
           return posts;
