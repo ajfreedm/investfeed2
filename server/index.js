383850b86ff49bf0e45f5c3339129d0fba0ecd47
iamDrewFreedman <exodus23_1@protonmail.com>
2022-10-07 18:15:29 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ajfreedm:mann2015*!@cluster0.rn83up6.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://ajfreedm:mann2015*!@cluster0.rn83up6.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
